37da88d4a316eff422235b82759b21073837ea87
blancagc <blanca.glez.c@hotmail.com>
2016-01-27 12:49:20 -0600

    05-Perimetros

---
+++ src/AppPerimetros.java
@@ -0,0 +1,11 @@
+
+public class AppPerimetros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Perimetros p = new Perimetros();
+		System.out.printf("El perimetro de un cuadrado es: %d", p.cuadrado(10));
+		
+	}
+
+}
---
+++ src/Perimetros.java
@@ -0,0 +1,8 @@
+
+public class Perimetros {
+	
+	public int cuadrado(int lado){
+		return lado + lado +lado + lado;
+		
+	}
+}
